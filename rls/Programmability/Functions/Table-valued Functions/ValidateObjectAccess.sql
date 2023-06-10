0868b0b0f22668b47bab4c0dea805f27d7155786
David Smith <kittoes0124@byteterrace.com>
2023-06-10 17:52:21 -0500

    refuctoring?

--- rls/Programmability/Functions/Table-valued Functions/ValidateObjectAccess.sql
+++ rls/Programmability/Functions/Table-valued Functions/ValidateObjectAccess.sql
@@ -6,49 +6,38 @@ returns table
 with native_compilation
    , schemabinding
 as return (
-    select a.[Value]
-    from (
-        select 1 -- allow users to view their own records
-        where exists (
-            select 1
-            from [rls].[Sessions] as ea
-            where (@@spid = ea.[Spid])
-              and (@userId = ea.[UserId])
-        )
-        union all
-        select 1 -- allow users with a SELECT grant to view all records
-        where exists (
-              select 1
-              from [rls].[Sessions] as ea
-              where exists (
-                    select 1
-                    from [rls].[Permissions] as eaa
-                    where (@objectId = eaa.[MajorId])
-                      and (0 = eaa.[MinorId])
-                      and ('SELECT' = eaa.[Name])
-                      and ('G' = eaa.[State])
-                      and (ea.[UserId] = eaa.[UserId])
-                )
-          )
-        union all
-        select 1 -- allow data readers to view all records
-        where (Convert(bit, 1) = is_member('db_datareader'))
-        union all
-        select 1 -- allow database owners to view all records
-        where (Convert(bit, 1) = is_member('db_owner'))
-    ) as a ([Value])
-    where (Convert(bit, 0) = is_member('db_denydatareader')) -- ensure that user isn't denied access to all records
-      and not exists (
-          select 1
-          from [rls].[Sessions] as ea
+    select 1 as [Value]
+    from [rls].[Sessions] as ea
+    where (@@spid = ea.[Spid])
+      and exists (
+          select 1 -- allow users access to their own records
+          where (@userId = ea.[UserId])
+          union all
+          select 1 -- allow users with a SELECT grant access to ALL records
           where exists (
                 select 1
                 from [rls].[Permissions] as eaa
                 where (@objectId = eaa.[MajorId])
                   and (0 = eaa.[MinorId])
                   and ('SELECT' = eaa.[Name])
-                  and ('D' = eaa.[State])
+                  and ('G' = eaa.[State])
                   and (ea.[UserId] = eaa.[UserId])
             )
+         union all
+         select 1 -- allow users who are members of db_datareader access to ALL records
+         where (Convert(bit, 1) = is_member('db_datareader'))
+         union all
+         select 1 -- allow users who are members of db_owner access to ALL records
+         where (Convert(bit, 1) = is_member('db_owner'))
+      )
+      and (Convert(bit, 0) = is_member('db_denydatareader')) -- block users who are members of db_denydatareader access to ALL records
+      and not exists ( -- block users with a SELECT deny access to ALL records
+          select 1
+          from [rls].[Permissions] as eaa
+          where (@objectId = eaa.[MajorId])
+            and (0 = eaa.[MinorId])
+            and ('SELECT' = eaa.[Name])
+            and ('D' = eaa.[State])
+            and (ea.[UserId] = eaa.[UserId])
       )
 );
