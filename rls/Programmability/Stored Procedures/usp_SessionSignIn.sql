09eee00a64cbe28c027b372e8c18327c305d58de
David Smith <kittoes0124@byteterrace.com>
2023-06-09 19:09:42 -0500

    added logic to override spid with new sign in

--- rls/Programmability/Stored Procedures/usp_SessionSignIn.sql
+++ rls/Programmability/Stored Procedures/usp_SessionSignIn.sql
@@ -8,6 +8,13 @@ begin;
 
     execute [sys].[sp_set_session_context]
         @key = N'object_id'
-      , @read_only = 1
       , @value = @objectId
+      , @read_only = 1;
+
+    delete a
+    from [rls].[Sessions] as a
+    where (@@spid = a.Spid);
+
+    insert into [rls].[Sessions] ([ObjectId])
+    select @objectId;
 end;
--- rls/Tables/Sessions.sql
+++ rls/Tables/Sessions.sql
@@ -2,7 +2,7 @@
     [Spid] smallint not null constraint [rls.Sessions_DftSpid] default (@@spid)
   , [ObjectId] varchar(36) not null
   , constraint [rls.Sessions_ChkSpid] check (@@spid = [Spid])
-  , constraint [rls.Sessions_Pk] primary key nonclustered hash ([Spid] asc) with (bucket_count = 131072)
+  , constraint [rls.Sessions_Pk] primary key nonclustered hash ([Spid]) with (bucket_count = 131072)
 ) with (
     durability = schema_only
   , memory_optimized = on
